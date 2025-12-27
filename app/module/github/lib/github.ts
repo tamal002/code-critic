e86ac3397f13da89e60d6f35a69a915cd7410a44
tamal002 <sahatamal66@gmail.com>
2025-12-27 22:47:46 +0530

    dashboard backend implemented

---
+++ app/module/github/lib/github.ts
@@ -0,0 +1,97 @@
+
+"use server";
+
+import { Octokit } from "octokit";
+import { auth } from "@/lib/auth";
+import prisma from "@/lib/db";
+import { headers } from "next/headers";
+
+/**
+ *
+ * * Get GitHub access token for the authenticated user
+ * * @returns {Promise<string | null>} GitHub access token or null if not found
+ *
+ */
+export const getGithubToken = async (): Promise<string | null> => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    throw new Error("unauthenticated");
+  }
+
+  const account = await prisma.account.findFirst({
+    where: {
+      userId: session.user.id,
+      providerId: "github",
+    },
+  });
+
+  if (!account?.accessToken) {
+    throw new Error("No GitHub account found");
+  }
+
+  return account.accessToken;
+};
+
+
+/**
+ * Fetch user contribution data from GitHub GraphQL API
+ * @param accessToken 
+ * @param username 
+ * @returns Contribution data
+ */
+export const fetchUserContribution = async (
+  accessToken: string,
+  username: string
+) => {
+  const octokit = new Octokit({
+    auth: accessToken,
+  });
+
+  const query = `
+    query($username: String!) {
+      user(login: $username){
+        contributionsCollection {
+            contributionCalendar {
+                totalContributions
+                weeks {
+                    contributionDays {
+                        date
+                        contributionCount
+                        color
+                    }
+                }
+            }
+        }
+      } 
+    }
+    `;
+
+    interface ContributionData {
+        user: {
+            contributionsCollection: {
+                contributionCalendar: {
+                    totalContributions: number;
+                    weeks: {
+                        forEach(arg0: (week: any) => void): unknown;
+                        contributionDays: {
+                            date: string | Date;
+                            contributionCount: number;
+                            color: string;
+                        };
+                    };    
+                };
+            };
+        }
+    }
+
+    try {
+        const response : ContributionData = await octokit.graphql(query, { username });
+        return response.user.contributionsCollection.contributionCalendar;
+    } catch (error) {
+        console.error("Error at fetchUserContribution:", error);
+        throw error;
+    }
+}
